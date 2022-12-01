621410705173bcc393ea7defd16444d31dbea712
MISHU\14702 <arabin.tappware@gmail.com>
2022-12-01 18:56:04 -0500

    Desc<Tescase class name changed>

--- app/src/androidTest/java/com/arabin/AlbertsonsAcronymsTest/AlbertSonsTestTestCase.kt
+++ app/src/androidTest/java/com/arabin/AlbertsonsAcronymsTest/AlbertSonsTestTestCase.kt
@@ -3,12 +3,11 @@ package com.arabin.AlbertsonsAcronymsTest
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.arabin.AlbertsonsAcronymsTest.fragments.EnterMacroFragment
 import junit.framework.Assert.assertEquals
-import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class AlbersonsTestCase {
+class AlbertSonsTestTestCase {
 
     @Test
     fun testNavOptionIsworkingFine() {
