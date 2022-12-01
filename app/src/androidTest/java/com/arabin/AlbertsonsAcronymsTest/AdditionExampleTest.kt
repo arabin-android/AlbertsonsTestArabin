efb83724e50b85adf32710eec6b652d5a06d1c8e
MISHU\14702 <arabin.tappware@gmail.com>
2022-12-01 17:13:15 -0500

    Desc<test case>

--- app/src/androidTest/java/com/arabin/AlbertsonsAcronymsTest/AdditionExampleTest.kt
+++ app/src/androidTest/java/com/arabin/AlbertsonsAcronymsTest/AdditionExampleTest.kt
@@ -1,14 +1,18 @@
 package com.arabin.AlbertsonsAcronymsTest
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.arabin.AlbertsonsAcronymsTest.fragments.EnterMacroFragment
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
 
-
+@RunWith(AndroidJUnit4::class)
 class AlbersonsTestCase {
 
     @Test
-    fun testNavOptionIsworkingFine(): Boolean{
-        return androidx.navigation.ui.R.anim.nav_default_enter_anim == EnterMacroFragment.getNavigationOptions()?.enterAnim
+    fun testNavOptionIsworkingFine(){
+        assertEquals(androidx.navigation.ui.R.anim.nav_default_enter_anim, EnterMacroFragment.getNavigationOptions()?.enterAnim)
     }
 
 }
